f638829f6c19914c1071dc198582cfe845228000
aparsh <aparsh.gupta@gmail.com>
2020-06-30 23:05:13 +0530

    mongoose population

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res, next) {
 router.post('/signup', (req,res,next) => {
   User.register(new User({username : req.body.username}),
   req.body.password, (err,user) => {
+    console.log(user);
     if(err)
     {
       res.statusCode = 500;
@@ -22,11 +23,26 @@ router.post('/signup', (req,res,next) => {
       res.json({err : err});
     }
     else{
-      passport.authenticate('local')(req,res,()=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json({success:true,status: 'Resgistration Successful!'});
-      })
+      if(req.body.firstname){
+        user.firstname = req.body.firstname;
+      }
+      if(req.body.lastname){
+        user.lastname = req.body.lastname;
+      }
+      user.save((err,user)=>{
+        if(err){
+          console.log('error2');
+          res.statusCode = 500;
+          res.setHeader('Content-Type','application/json');
+          res.json({err : err});
+          return;
+        }
+        passport.authenticate('local')(req,res,()=>{
+          res.statusCode = 200;
+          res.setHeader('Content-Type','application/json');
+          res.json({success:true,status: 'Resgistration Successful!'});
+        });
+      });
     }
   });
 });
